07cf2b7caf8f5fed25f3458322708df651fd462e
thabisomaqhawengwenya <thabisomaqhawengwenya@gmail.com>
2026-08-10 20:09:02 +0200

    added android wrapper

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.emganwinisda.app',
+  appName: 'Emganwini Main SDA Church',
+  webDir: 'dist'
+};
+
+export default config;
